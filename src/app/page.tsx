48501959de2b011f6311fabb955f9d4d46aacf05
Parisa Esmaeilpour <esmaeilpour.parisa@gmail.com>
2025-03-10 13:00:05 +0330

    fix: base structure of project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Footer from "@/components/home/Footer";
+import Header from "@/components/home/Header";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
